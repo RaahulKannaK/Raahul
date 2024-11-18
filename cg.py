516d0fb137400ef8fa5951603fc3845874032111
RaahulKannaK <raahulkannak@gmail.com>
2024-11-18 08:48:23 +0530

    Add files via upload

---
+++ cg.py
@@ -0,0 +1,45 @@
+import streamlit as ra
+import random
+from PIL import Image
+
+def guess():
+    g = random.randint(1, 100)
+    image = Image.open(r'C:\Users\raahu\Downloads\guess2.png')
+    image1 = Image.open(r'C:\Users\raahu\Downloads\guess4.png')
+    c1,c2 = ra.columns(2)
+    with c1:
+        ra.image(image, caption="Left Aligned Image", width=350)
+    with c2:
+        ra.image(image1, caption="Right Aligned Image",  width=350)
+    attempts = 0
+    ra.header("Guessing Game By Computer")
+    ra.write("Guess a Number between the Range of 1 and 100.")
+
+
+    with ra.form("guessing_form"):
+        u = ra.number_input("What is the Number you give for Computer?", min_value=1, max_value=100)
+        submit_button = ra.form_submit_button(label='Submit')
+
+        if submit_button:
+            
+            difference = abs(g - u)
+            
+
+            if u == g:
+                ra.success(f"Congratulations! You Guessed the Correct Number with {attempts} attempts.")
+            elif difference <= 10 or difference < 5:
+                ra.write("Computer Guess is",g)
+                ra.write("That’s almost it, just a little more")
+            elif u < g:
+                ra.write("Computer Guess is",g)
+                ra.write("It is High So, try a bit lower.")
+            else:
+                ra.write("Computer Guess is",g)
+                ra.write("It is Low So, try a bit higher")
+
+def title():
+    ra.title("Guessing Game PLatform 🤔")
+    guess()
+
+if __name__ == "__main__":
+    title()
---
+++ ug.py
@@ -0,0 +1,46 @@
+import streamlit as ra
+import random
+from PIL import Image
+
+def guess():
+    g = random.randint(1, 100)
+    image = Image.open(r'C:\Users\raahu\Downloads\guess2.png')
+    image1 = Image.open(r'C:\Users\raahu\Downloads\guess.png')
+    c1,c2 = ra.columns(2)
+    with c1:
+        ra.image(image, caption="Left Aligned Image", width=350)
+    with c2:
+        ra.image(image1, caption="Right Aligned Image",  width=350)
+
+    ra.header("Guessing Game by Computer")
+    ra.write("The Computer Guess",g)
+    ra.write("Guess a Number between the Range of 1 and 100.")
+
+    with ra.form("guessing_form"):
+        u = ra.number_input("What Is your Guess? Enter Below!:", min_value=1, max_value=100)
+        con = ra.form_submit_button(label='Confirm')
+
+        if con:
+            
+            diff = abs(g - u)
+            
+
+            if u == g:
+                ra.success("Congratulations! You got the correct Guess.")
+                ra.write(diff)
+            elif diff <= 10 or diff < 5:
+                ra.write("That's almost it, just a little more")
+                ra.write("Difference Between your Guess and Computer Guess",diff)
+
+            elif u < g:
+                ra.write("It is Lower So, try a bit higher.")
+                ra.write("Difference Between your Guess and Computer Guess",diff)
+            else:
+                ra.write("It is Higher So, try a bit Lower")
+                ra.write("Difference Between your Guess and Computer Guess",diff)
+
+def T():
+    ra.title("Guessing Game PLatform 🤔")
+    guess()
+if __name__ == "__main__":
+    T()
